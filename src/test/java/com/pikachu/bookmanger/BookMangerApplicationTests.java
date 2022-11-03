b8e023ce4046e49521410ad0b060e0eb2fc29e7c
pikachu <pikachu029@163.com>
2022-11-03 22:24:59 +0800

    Initial commit

---
+++ src/test/java/com/pikachu/bookmanger/BookMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pikachu.bookmanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
